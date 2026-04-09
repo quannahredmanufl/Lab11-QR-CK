93e90ee1f01e32b7b8233b1e9308d10628c41df1
Quannah Redman <quannahredman@ufl.edu>
2026-04-09 10:59:22 -0400

    calculator modified

--- calculator.py
+++ calculator.py
@@ -6,6 +6,19 @@ One function per operation, in order.
 """
 # First example
 import math
+def square_root(a):
+    try:
+        if a < 0:
+            raise ValueError("Cannot take square root of a negative number")
+        return math.sqrt(a)
+    except ValueError as e:
+        raise e
+
+def hypotenuse(a, b):
+    try:
+        return math.hypot(a, b)
+    except Exception as e:
+        raise e
 
 def add(a, b): a + b
 
