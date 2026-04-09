e50e8f78fa573739d4d8f276fba5410b71310e64
Cole Knaus <coleknaus@Coles-MacBook-Air.local>
2026-04-09 10:37:24 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -3,6 +3,10 @@ calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
+
+# https://github.com/quannahredmanufl/Lab11-QR-CK.git
+# Partner 1: Quannah Redman
+# Partner 2: Cole Knaus
 """
 # First example
 def add(a, b): 
