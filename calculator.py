61b427338ab15300d6ddeb6c1ea1d6c78d47f2b0
Cole Knaus <c.knaus@ufl.edu>
2026-04-09 11:06:52 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -5,6 +5,40 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b):
-    pass
+import math
+def square_root(a):
+    try:
+        if a < 0:
+            raise ValueError("Cannot take square root of a negative number")
+        return math.sqrt(a)
+    except ValueError as e:
+        raise e
+
+def hypotenuse(a, b):
+    try:
+        return math.hypot(a, b)
+    except Exception as e:
+        raise e
+
+def add(a, b): a + b
+
+def subtract(a, b): a - b
+
+def multiply(a, b): a * b
+
+def divide(a, b):
+    if a == 0:
+        print("ZeroDivisionError")
+    else:
+        b / a
+
+def logarithm(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("ValueError")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
+
+
 
